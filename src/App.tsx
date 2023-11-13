09627dcdf8ae72c525fe9023db6be9bd8c018abe
Victor Santele <victor.santele@gmail.com>
2023-11-13 17:14:14 +0100

    Add SharkChoice component and navigation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css"
 import Header from "./components/header/header"
 import Navigation from "./components/navigation/navigation"
 import PimpMyRequin from "./components/pimpMyRequin/pimpMyRequin"
+import SharkChoice from "./components/sharkChoice/charkChoice"
 
 function App() {
   const [activeTab, setActiveTab] = useState(0)
@@ -12,11 +13,27 @@ function App() {
     setActiveTab(newTab)
   }
 
+  const nextTab = () => {
+    setActiveTab((a) => {
+      if (a >= 3) return a
+      return a + 1
+    })
+  }
+  const previousTab = () => {
+    setActiveTab((a) => {
+      if (a <= 0) return a
+      return a - 1
+    })
+  }
+
   return (
     <>
       <Header activeTab={activeTab} />
-      <div className="body">{activeTab === 0 && <PimpMyRequin />}</div>
-      <Navigation />
+      <div className="body">
+        {activeTab === 0 && <PimpMyRequin />}
+        {activeTab === 1 && <SharkChoice />}
+      </div>
+      <Navigation onNext={nextTab} onPrevious={previousTab} />
     </>
   )
 }

574c2313509bd258f41996a30c75e75e9e4a27e1
Victor Santelé <victor.santele@gmail.com>
2023-11-30 15:32:59 +0100

    Play jingle first time load session

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import "./App.css"
 import Header from "./components/header/header"
 import Navigation from "./components/navigation/navigation"
@@ -5,9 +6,24 @@ import PimpMyRequin from "./components/pimpMyRequin/pimpMyRequin"
 import SharkChoice from "./components/sharkChoice/sharkChoice"
 import WhereIsMyRequin from "./components/whereIsMyRequin/whereIsMyRequin"
 import { useNavigationContext } from "./contexes/navigationContext"
+import jingleSrc from "./assets/jingle.mp3"
+import useSessionState from "./utils/useSessionState"
 
 function App() {
   const { selectedTab, nextTab, previousTab } = useNavigationContext()
+
+  const [isJinglePlayed, setIsJinglePlayed] = useSessionState(
+    "isJinglePlayed",
+    false
+  )
+
+  useEffect(() => {
+    if (!isJinglePlayed) {
+      const audio = new Audio(jingleSrc)
+      audio.play()
+      setIsJinglePlayed(true)
+    }
+  }, [isJinglePlayed, setIsJinglePlayed])
 
   return (
     <>

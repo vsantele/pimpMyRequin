4f6a3e6c04889362ef2550dfc299ac32c2545658
Victor Santelé <victor.santele@gmail.com>
2023-12-10 21:30:31 +0100

    Add info to the journey

--- src/components/journey/journey.tsx
+++ src/components/journey/journey.tsx
@@ -48,16 +48,18 @@ export default function Journey() {
   }
 
   return (
-    <div style={{ display: "flex", alignItems: "flex-start" }}>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
       <div style={{ width: "60vw", height: "60vh" }}>
         <MapContainer initMap={initMap}>
           {sharks.map((shark) => (
             <Marker
               key={shark.caseNumber}
               position={[shark.latitude, shark.longitude]}
-            >
-              Tests
-            </Marker>
+            ></Marker>
           ))}
           <Polyline
             positions={sharks.map((shark) => [shark.latitude, shark.longitude])}
@@ -65,9 +67,82 @@ export default function Journey() {
           />
         </MapContainer>
       </div>
-      <button style={{ marginLeft: "1rem" }} onClick={() => setSelectedTab(0)}>
-        Nouvelle partie
-      </button>
+      <div
+        style={{
+          marginLeft: "1rem",
+          backgroundColor: "whitesmoke",
+          padding: "1rem",
+          color: "black",
+        }}
+      >
+        <div>
+          <h2>Type de trajet</h2>
+          <label style={{ marginRight: "1rem" }} htmlFor="shortestDistance">
+            <input
+              type="radio"
+              id="shortestDistance"
+              name="distance"
+              checked={false}
+            />
+            Le plus court
+          </label>
+          <label htmlFor="cheapestDistance">
+            <input
+              type="radio"
+              id="cheapestDistance"
+              name="distance"
+              checked={true}
+            />
+            Le moins cher
+          </label>
+        </div>
+        <h2 style={{ marginLeft: "1rem", textAlign: "start" }}>
+          Distance à parcourir
+        </h2>
+        <p style={{ marginLeft: "1rem" }}>
+          {getDistance(
+            sharks.map((s) => [s.latitude, s.longitude])
+          ).toLocaleString()}{" "}
+          kilomètres
+        </p>
+        <h2 style={{ marginLeft: "1rem", textAlign: "start" }}>Coût</h2>
+        <p style={{ marginLeft: "1rem" }}>
+          {getCost(
+            getDistance(sharks.map((s) => [s.latitude, s.longitude]))
+          ).toLocaleString()}{" "}
+          €
+        </p>
+        <button style={{ marginTop: "8rem" }} onClick={() => setSelectedTab(0)}>
+          Nouvelle partie
+        </button>
+      </div>
     </div>
   )
+}
+
+function getDistance(coords: [number, number][]) {
+  let distance = 0
+  for (let i = 0; i < coords.length - 1; i++) {
+    distance += distanceBetween(coords[i], coords[i + 1])
+  }
+  return distance
+}
+
+function distanceBetween(coord1: [number, number], coord2: [number, number]) {
+  const R = 6371e3 // metres
+  const φ1 = (coord1[0] * Math.PI) / 180 // φ, λ in radians
+  const φ2 = (coord2[0] * Math.PI) / 180
+  const Δφ = ((coord2[0] - coord1[0]) * Math.PI) / 180
+  const Δλ = ((coord2[1] - coord1[1]) * Math.PI) / 180
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  return Math.round((R * c) / 1000) // in kilometres
+}
+
+function getCost(distance: number) {
+  return Math.round(distance * 0.42)
 }
